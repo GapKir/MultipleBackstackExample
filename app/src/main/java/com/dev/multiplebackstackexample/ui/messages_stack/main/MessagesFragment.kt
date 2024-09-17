e3847672badb635e3630ccb703b293fa92734447
Kirill <kirillgapochka55@gmail.com>
2024-09-17 13:33:12 +0300

    My commit commit

--- app/src/main/java/com/dev/multiplebackstackexample/ui/messages_stack/main/MessagesFragment.kt
+++ app/src/main/java/com/dev/multiplebackstackexample/ui/messages_stack/main/MessagesFragment.kt
@@ -13,6 +13,7 @@ import com.dev.multiplebackstackexample.utills.openFragment
 
 //first commit
 //second commit
+//my commit
 class MessagesFragment: Fragment(R.layout.fragment_messages), MessagesAdapter.Listener {
 
     private var _binding: FragmentMessagesBinding? = null
